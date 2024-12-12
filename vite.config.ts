bfa7bc496e851acadac034ab662e6e7231f0554e
elvinsavio <elvinsavio.lp@gmail.com>
2024-12-13 00:21:14 +0530

    replace vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,14 @@
-// vite.config.js
-export default {
-    base: '/elvinsavio.github.io'
-  }
+import { reactRouter } from "@react-router/dev/vite";
+import autoprefixer from "autoprefixer";
+import tailwindcss from "tailwindcss";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [tailwindcss, autoprefixer],
+    },
+  },
+  plugins: [reactRouter(), tsconfigPaths()],
+});
