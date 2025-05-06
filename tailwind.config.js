2f0eb1e925dcdf1b20c116e2d98f33885ca1b1d2
elvinsavio <elvinsavio.lp@gmail.com>
2025-05-06 23:25:58 +0530

    init

---
+++ tailwind.config.js
@@ -0,0 +1,51 @@
+module.exports = {
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    content: ['./templates/**/*.html', './templates/*.html'],
+    theme: {
+      extend: {
+        fontFamily: {},
+        colors: {
+          border: "var(--border)",
+          input: "var(--input)",
+          ring: "hsl(var(--ring))",
+          background: "var(--background)",
+          foreground: "var(--foreground)",
+          primary: {
+            DEFAULT: "var(--primary)",
+            foreground: "var(--primary-foreground)",
+          },
+          secondary: {
+            DEFAULT: "var(--secondary)",
+            foreground: "var(--secondary-foreground)",
+          },
+          destructive: {
+            DEFAULT: "var(--destructive)",
+            foreground: "var(--destructive-foreground)",
+          },
+          muted: {
+            DEFAULT: "var(--muted)",
+            foreground: "var(--muted-foreground)",
+          },
+          accent: {
+            DEFAULT: "var(--accent)",
+            foreground: "var(--accent-foreground)",
+          },
+          popover: {
+            DEFAULT: "var(--popover)",
+            foreground: "var(--popover-foreground)",
+          },
+          card: {
+            DEFAULT: "var(--card)",
+            foreground: "var(--card-foreground)",
+          },
+        },
+        borderRadius: {
+          xl: "calc(var(--radius) + 4px)",
+          lg: "var(--radius)",
+          md: "calc(var(--radius) - 2px)",
+          sm: "calc(var(--radius) - 4px)",
+        },
+      },
+    },
+  }
